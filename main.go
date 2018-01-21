fc64536da7057fa2691c926e3456bf9d58601dad
Marc Audefroy <marc.audefroy@corp.ovh.com>
2018-01-21 20:57:59 +0100

    add beez and bar dependency

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+
+	bar "github.com/marcaudefroy/projectB/models"
+	beez "github.com/marcaudefroy/projectC/models"
 )
 
 func main() {
+	_ = bar.Bar{}
+	_ = beez.Beez{}
+
 	fmt.Println("project A")
 }
---
+++ vendor/github.com/marcaudefroy/projectB/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/marcaudefroy/projectC/models"
+)
+
+func main() {
+	fmt.Println("project B")
+	_ = models.Beez{}
+}
